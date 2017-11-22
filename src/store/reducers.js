5af6c65ff011394d4434cd9786b9fa3bc3596ba4
Neras <medianeras57@gmail.com>
2017-11-22 22:03:26 +0800

    add prop-types

---
+++ src/store/reducers.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions';
+const { SHOW_ALL } = VisibilityFilters;
+
+function visibilityFilter (state = SHOW_ALL, action) {
+	switch(action.type) {
+		case SET_VISIBILITY_FILTER:
+			return action.filter;
+		default:
+			return state;
+	}
+}
+
+function todos(state = [], action) {
+	switch(action.type) {
+		case ADD_TODO:
+			return [
+				...state,
+				{
+					text: action.text,
+					completed: false
+				}
+			]
+	}
+}
