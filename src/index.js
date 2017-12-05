da4d6edf64d405eb2cdef840e4e66f136aea990f
Neras <medianeras57@gmail.com>
2017-12-05 17:35:13 +0800

    change files

--- src/index.js
+++ src/index.js
@@ -1,40 +1,25 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './style.css';
-//
-// import Header from './components/common/Header/';
-//
-// class Index extends React.Component{
-//     constructor(props){
-//         super(props);
-//     }
-//
-//     render(){
-//         return (
-//             <div className="resume-div__wrapper">
-//                 <Header />
-//             </div>
-//         );
-//
-//     }
-// }
-//
-// ReactDOM.render(
-//     <Index />,
-//     document.getElementById('app')
-// )
 import React from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-import todoApp from './store/reducers';
+import ReactDOM from 'react-dom';
+import './style.css';
 
-let store = createStore(todoApp);
-let rootElement = document.getElementById('app');
-render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	rootElement
-);
+import Header from './components/common/Header/';
+
+class Index extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="resume-div__wrapper">
+                <Header />
+            </div>
+        );
+
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('app')
+)
