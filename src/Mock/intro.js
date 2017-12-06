54c7c624186a264710d149cdfd0a547942729156
Neras <medianeras57@gmail.com>
2017-12-06 23:10:23 +0800

    add mock data(temporary)

---
+++ src/Mock/intro.js
@@ -0,0 +1,19 @@
+export default {
+   intro:[
+       {
+           key:'name',
+           label: '' ,
+           val: '蒋正兴'
+       },
+       {
+           key:'sckool',
+           label: '',
+           val:'毕业于重庆邮电大学'
+       },
+       {
+           key:'CET',
+           label: '',
+           val: '英语考过了CET4'
+       }
+   ]
+}
