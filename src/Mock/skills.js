35197496fe9b2ae2e5f8bee4e568bb4b68490a53
Neras <medianeras57@gmail.com>
2018-01-09 15:43:39 +0800

    change skills data

--- src/Mock/skills.js
+++ src/Mock/skills.js
@@ -7,7 +7,7 @@ export default {
         '- 熟练使用PhotoShop进行切图，最大还原设计稿',
         '- 使用Node进行后台开发，并有上线项目',
         '- 使用VUE进行开发，并有上线项目',
-        '- 了解Webpack,React,Canvas,Java,Python,Nginx',
+        '- 了解Webpack,React,Canvas,Python,Nginx',
         '- 了解小程序开发'
     ]
 }
